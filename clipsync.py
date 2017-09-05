ff5cde9f454614e5f43879f51a6fb49ffc71354b
Stephen G Lynch <stepheng.lynch@gmail.com>
2017-09-05 11:48:06 +0100

    added some sockets code

--- clipsync.py
+++ clipsync.py
@@ -1,17 +1,75 @@
-#/bin/env python3
+#/bin/env python
+
+import socket
+import threading
+import time
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
 
-def print_something(board, event):
+UDP_ADDRESS_BC = ('<broadcast>', 50002)
+
+
+class ClipSync:
+
+    def __init__(self):
+
+        #setup clipboard
+        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        self._response = False
+
+    def _broadcast_thread(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) #enable broadcast
+        s.connect(UDP_ADDRESS_BC)
+
+        #loops until there is a response from listening thread
+        while not self._response:
+            s.send('clipsync:broadcast')
+            time.sleep(0.25)
+
+    def _listener_thread(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(UDP_ADDRESS_BC)
+
+        while True:
+            data = s.recv(50)
+            if data == 'clipsync:response':
+                self._response = True
+                break
+
+    def find_a_device(self):
+        """
+        Finds first device that responds
+        """
+
+        self._response = False
+
+        #Create and start threads
+        bc_thread = threading.Thread(target=self._broadcast_thread)
+        list_thread = threading.Thread(target=self._listener_thread)
+        bc_thread.start()
+        list_thread.start()
+        bc_thread.join()
+        list_thread.join()
+
+        return ['TODO:return list of devices']
+
+
+def update_phone_clipboard(board, event):
+    #TODO: This needs to properly fleshed
     print('Clipboard changed')
-    print(board, event)
+    print(board.wait_for_text())
     print('')
 
     
 if __name__ == '__main__':
-    clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
-    clipboard.connect('owner-change', print_something)
-    Gtk.main()
+
+    print 'started'
+
+    cs = ClipSync()
+    cs.find_a_device()
+
+    print 'finished'
