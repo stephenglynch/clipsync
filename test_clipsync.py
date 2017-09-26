fc9abc2b7caca2444ffc0b2ee003de15ca80687c
Stephen G Lynch <stepheng.lynch@gmail.com>
2017-09-26 16:59:34 +0100

    added testing file

--- test_clipsync.py
+++ test_clipsync.py
@@ -1,5 +1,14 @@
 #!/bin/env python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Copyright (c) 2017 Stephen G. Lynch
+
+# Contribitors:
+#   Stephen G. Lynch <stepheng.lynch@gmail.com>
+
 import socket
 import threading
 import time
